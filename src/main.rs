709a1991e824d6348ae45663f653552ba6544495
Kek5chen <kx@imkx.dev>
2024-03-24 13:53:41 +0100

    fix: actually make it pseudo-infinite

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn init_simulation(args: &Vec<String>) -> Result<Simulation, Box<dyn Error>> {
         .map_err(|_| "Could not parse time_to_sleep")?;
     let nb_meals: u32 = match args.get(5) {
         Some(nb_meals) => nb_meals.parse()?,
-        None => 10,
+        None => u32::MAX,
     };
 
     Ok(Simulation::init(nb_philo, time_to_die, time_to_eat, time_to_sleep, nb_meals))
