ee837a9f313e511e0bcc3c18169ff9bdf11941b6
Kek5chen <kx@imkx.dev>
2024-03-24 11:14:03 +0100

    feat: philo thread doin philo stuff

--- src/simulation.rs
+++ src/simulation.rs
@@ -1,23 +1,35 @@
+use std::cell::Cell;
 use std::error::Error;
-use std::sync::{Arc, Mutex};
+use std::ops::Sub;
+use std::sync::{Arc, Mutex, RwLock};
+use std::sync::atomic::{AtomicU32, AtomicUsize};
+use std::sync::atomic::Ordering::*;
 use std::thread;
-use std::thread::{JoinHandle, Thread};
+use std::thread::{current, JoinHandle, sleep, Thread};
+use std::time::{Duration, Instant};
 
 struct SharedPhiloData {
     time_to_die: u32,
     time_to_eat: u32,
     time_to_sleep: u32,
-    nb_meals: u32
+    nb_meals: u32,
+    start_time: RwLock<Instant>
 }
 
 struct PhiloData {
-    meals_eaten: u32
+    num: AtomicUsize,
+    meals_eaten: AtomicU32,
+    last_eat: Mutex<Instant>,
+    forks: [Arc<Mutex<()>>; 2]
 }
 
-impl Default for PhiloData {
-    fn default() -> Self {
+impl PhiloData {
+    fn new(num: usize, left_fork: Arc<Mutex<()>>, right_fork: Arc<Mutex<()>>) -> PhiloData {
         PhiloData {
-            meals_eaten: 0
+            num: AtomicUsize::new(num),
+            meals_eaten: AtomicU32::new(0),
+            last_eat: Mutex::new(Instant::now()),
+            forks: [left_fork, right_fork]
         }
     }
 }
@@ -26,30 +38,41 @@ pub struct Simulation {
     nb_philo: u32,
     shared_data: Arc<SharedPhiloData>,
     owned_data: Vec<Arc<PhiloData>>,
+    forks: Vec<Arc<Mutex<()>>>,
 }
 
 impl Simulation {
     pub fn init(nb_philo: u32, time_to_die: u32, time_to_eat: u32, time_to_sleep: u32, nb_meals: u32) -> Self {
+        let forks = (0..nb_philo).map(|_| Arc::new(Mutex::new(()))).collect();
+
         Simulation {
             nb_philo,
             shared_data: Arc::new(SharedPhiloData {
                 time_to_die,
                 time_to_eat,
                 time_to_sleep,
-                nb_meals
+                nb_meals,
+                start_time: RwLock::new(Instant::now())
             }),
-            owned_data: Vec::new()
+            owned_data: Vec::new(),
+            forks,
         }
     }
 
     pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
-        let mut philos: Vec<(Arc<Philosopher>, JoinHandle<u32>)> = Vec::with_capacity(self.nb_philo as usize);
+        println!("Starting simulation..");
+        let mut philos: Vec<(Arc<Philosopher>, JoinHandle<()>)> = Vec::with_capacity(self.nb_philo as usize);
 
         self.owned_data.reserve(self.nb_philo as usize);
-        for i in 0..self.nb_philo {
-            let philo_data = Arc::new(PhiloData::default());
+        *self.shared_data.start_time.write().unwrap() = Instant::now();
+        for i in 0usize..self.nb_philo as usize {
+            let left_fork = self.forks[i].clone();
+            let right_fork = self.forks[(i + 1) % self.forks.len()].clone();
+
+            let philo_data = Arc::new(PhiloData::new(i, left_fork, right_fork));
             self.owned_data.push(philo_data.clone());
-            let philo = Philosopher::create(i, self.shared_data.clone(), philo_data);
+
+            let philo = Philosopher::create(self.shared_data.clone(), philo_data);
             philos.push(philo);
         }
 
@@ -60,13 +83,23 @@ impl Simulation {
     }
 }
 
-pub struct Philosopher {
+enum PhiloAction {
+    PickLeftFork,
+    PickRightFork,
+    Eat,
+    DropLeftFork,
+    DropRightFork,
+    Sleep,
+    WakeUp,
+}
+
+struct Philosopher {
     sim_data: Arc<SharedPhiloData>,
     data: Arc<PhiloData>,
 }
 
-impl Philosopher {
-    fn create(philo_num: u32, sim_data: Arc<SharedPhiloData>, philo_data: Arc<PhiloData>) -> (Arc<Philosopher>, JoinHandle<u32>) {
+impl<'a> Philosopher {
+    fn create(sim_data: Arc<SharedPhiloData>, philo_data: Arc<PhiloData>) -> (Arc<Philosopher>, JoinHandle<()>) {
         let mut philo = Arc::new(Philosopher {
             sim_data,
             data: philo_data,
@@ -74,7 +107,7 @@ impl Philosopher {
 
         let philo_clone = philo.clone();
         let handle = thread::Builder::new()
-            .name(format!("Philosopher {}", philo_num))
+            .name(format!("Philosopher {}", philo.data.num.load(Relaxed)))
             .spawn(move || {
                 philo_clone.live()
         }).unwrap();
@@ -83,8 +116,45 @@ impl Philosopher {
         (philo, handle)
     }
 
-    fn live(&self) -> u32 {
-        println!("Henlo from {}", thread::current().name().unwrap_or("Philosopher X"));
-        0
+    fn print_action(&self, action: PhiloAction) {
+        let current_time = self.sim_data.start_time.read().unwrap().elapsed();
+        let action_text = match action {
+            PhiloAction::PickLeftFork => "picked up the left fork",
+            PhiloAction::PickRightFork => "picked up the right fork",
+            PhiloAction::Eat => "started eating",
+            PhiloAction::DropLeftFork => "dropped the left fork",
+            PhiloAction::DropRightFork => "dropped the right fork",
+            PhiloAction::Sleep => "fell asleep",
+            PhiloAction::WakeUp => "woke up",
+        };
+
+        println!("{}\t\tphilo [{}]  {}", current_time.as_millis(), self.data.num.load(Relaxed), action_text)
+    }
+
+    fn live(&self) {
+        *self.data.last_eat.lock().unwrap() = Instant::now();
+        for i in 0..self.sim_data.nb_meals {
+            // pick up forks
+            let lock = self.data.forks[0].lock();
+            self.print_action(PhiloAction::PickLeftFork);
+            let lock2 = self.data.forks[1].lock();
+            self.print_action(PhiloAction::PickRightFork);
+
+            // eat
+            self.print_action(PhiloAction::Eat);
+            sleep(Duration::from_millis(self.sim_data.time_to_eat as u64));
+            *self.data.last_eat.lock().unwrap() = Instant::now();
+            self.data.meals_eaten.fetch_add(1, Relaxed);
+
+            // drop forks
+            drop(lock);
+            self.print_action(PhiloAction::DropLeftFork);
+            drop(lock2);
+            self.print_action(PhiloAction::DropRightFork);
+
+            // sleep
+            self.print_action(PhiloAction::Sleep);
+            sleep(Duration::from_millis(self.sim_data.time_to_sleep as u64));
+        }
     }
 }
