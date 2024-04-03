059777384f0461f60749d05e75bf24e46b2046d0
Kek5chen <kx@imkx.dev>
2024-04-03 19:43:57 +0200

    refactor: moved window creation into create_window

--- src/app/app.rs
+++ src/app/app.rs
@@ -1,25 +1,44 @@
 use std::error::Error;
+use std::ffi::{c_char, CStr, CString};
+use ash::extensions::khr;
+use ash::{Entry, vk};
+use ash_window::enumerate_required_extensions;
 use winit::event_loop::EventLoop;
 use winit::window::WindowBuilder;
+use raw_window_handle::{HasRawDisplayHandle};
 
 pub struct App {
+    event_loop: Option<EventLoop<()>>,
+    window: Option<winit::window::Window>,
     entry: ash::Entry,
 }
 
 impl App {
     pub fn new(app_name: &str, window_width: u32, window_height: u32) -> Result<Self, Box<dyn Error>> {
         unsafe {
-            let event_loop = EventLoop::new()?;
-            let window = WindowBuilder::new()
-                .with_title(app_name)
-                .with_inner_size(winit::dpi::LogicalSize::new(
-                    f64::from(window_width),
-                    f64::from(window_height),
-                ))
-                .build(&event_loop)
-                .unwrap();
+            let mut app = App {
+                event_loop: None,
+                window: None,
+                entry: Entry::linked()
+            };
 
-            Ok(App{entry})
+            app.create_window(app_name, window_width, window_height)?;
+            Ok(app)
         }
+    }
+
+    fn create_window(&mut self, title: &str, width: u32, height: u32) -> Result<(), Box<dyn Error>> {
+        let event_loop = EventLoop::new()?;
+        self.window = Some(WindowBuilder::new()
+            .with_title(title)
+            .with_inner_size(winit::dpi::LogicalSize::new(
+                width,
+                height,
+            ))
+            .build(&event_loop)
+            .unwrap());
+        self.event_loop = Some(event_loop);
+
+        Ok(())
     }
 }
