84bbff88e92f1e5135fb86cdea8ed721849f2b6e
Kek5chen <kx@imkx.dev>
2024-04-04 11:19:59 +0200

    fix: exit with proper exit code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 mod app;
 
+use std::process::exit;
 use std::time::Duration;
 use crate::app::App;
 
 fn main() {
-    let app = App::new("Funky App", 800, 600)
-        .expect("oh no..?");
+    let app = match App::new("Funky App", 800, 600) {
+        Err(e) => { eprintln!("{}", e); exit(1); },
+        _ => (),
+    };
     std::thread::sleep(Duration::from_secs(10));
     drop(app);
     std::thread::sleep(Duration::from_secs(10));
